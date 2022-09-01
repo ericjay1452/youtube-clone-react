2ac162e6472183be7baa0242af0be396eaf11f78
Ilesanmi-Erioluwa <codeineboy1452@gmail.com>
2022-09-01 19:04:14 +0100

    created some Components

--- src/Components/Feed/Videos.jsx
+++ src/Components/Feed/Videos.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Stack, Box } from '@mui/material'
 
 const Videos = ({videos}) => {
-    console.log({videos})
+    
   return (
-    <div>Videos</div>
+    <Stack
+    direction={"row"}
+    justifyContent = {"start"}
+    flexWrap = {"wrap"}
+    gap = {2}
+    >
+   {videos.map( (item, index) =>(
+    <Box key = {index}>
+        {/* Checking if item id is Video format, then render videos */}
+        {item.id.videoId && <VideoCard video = {item} />}
+
+        {/* Checking if type of Id = channel, then display Channel Details */}
+        {item.id.channelId && <ChannelCard channelDetail = {item} />}
+    </Box>
+   ))}
+    </Stack>
   )
 }
 
