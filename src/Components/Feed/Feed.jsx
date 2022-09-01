6f648a2c4b43c806389638b75ef872759e5ea49d
Ilesanmi-Erioluwa <codeineboy1452@gmail.com>
2022-09-01 16:24:32 +0100

    Working on Feed component

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import {Stack, Box, Typography} from "@mui/material"
+
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <Stack
+    // For creating responsiveness on devices sm, md e.t.c (i.e code in sx)
+    sx = {{flexDirection: {sx: "column", md : "row"}}}
+    >
+     <Box 
+     
+    // For creating responsiveness on devices sm, md e.t.c (i.e code in sx)
+    sx = {{height: {sx: "auto", md : "92vh"}, }}
+     >
+
+     </Box>
+    </Stack>
   )
 }
 
