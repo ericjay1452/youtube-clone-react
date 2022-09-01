6546743e6621bcaa52f0c475b5cfe39eb77b1eec
Ilesanmi-Erioluwa <codeineboy1452@gmail.com>
2022-09-01 17:24:27 +0100

    still working on Video ontent

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -7,7 +7,7 @@ const Feed = () => {
   // created a two dimensional layout 
   // 1.for SideBar
   // 2. for Videos Content
-  
+
   return (
     <Stack
     // For creating responsiveness on devices sm, md e.t.c (i.e code in sx)
@@ -17,7 +17,7 @@ const Feed = () => {
      
     // For creating responsiveness on devices sm, md e.t.c (i.e code in sx)
     sx = {{height: {sx: "auto", md : "92vh"}, 
-          borderRight: "1px solid 3d3d3d",
+          borderRight: "1px solid #3d3d3d",
           px : {sx : 0, md : 2}}}
      >
 
@@ -31,7 +31,13 @@ const Feed = () => {
      </Typography>
      </Box>
 
-     {/* For  */}
+     {/* For  Videos Content*/}
+     <Box p= {2} sx = {{overflowY : "auto", height : "90vh", flex : 2}}>
+        <Typography color={"white"} mb = {2} variant = {'h4'} fontWeight = "bold">
+        New <span style={{color : "orange"}}>Video</span>
+        </Typography>
+     </Box>
+
     </Stack>
   )
 }
