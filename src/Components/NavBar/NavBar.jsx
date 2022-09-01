5ced1f665fcca9ae8c7832df85e09f800f7b33a1
Ilesanmi-Erioluwa <codeineboy1452@gmail.com>
2022-09-01 15:45:10 +0100

    working on my NavBar

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import {Link} from "react-router-dom"
+import { Stack } from '@mui/material'
+import Logo from "../../Utils/Logo/Youtube-Logo.svg"
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <Stack
+    direction={"row"}
+    sx = {{background : "#000", position : "sticky", top : "0"}}
+    alignItems = {"center"}
+    p= {2}
+    justifyContent = {"space-between"}
+    >
+
+    </Stack>
   )
 }
 
