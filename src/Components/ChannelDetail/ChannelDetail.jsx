3e77e1496a2e9dd5eadc09cb7c90f0795d09a301
Ilesanmi-Erioluwa <codeineboy1452@gmail.com>
2022-09-02 13:24:09 +0100

    Still working

--- src/Components/ChannelDetail/ChannelDetail.jsx
+++ src/Components/ChannelDetail/ChannelDetail.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import { useParams } from 'react-router-dom'
-import { Box, } from '@mui/material'
+import { Box, CardMedia} from '@mui/material'
 import {Videos, ChannelCard} from "../../Components"
 import { YoutubeApi } from '../../Utils/YoutubeApi'
 
@@ -10,15 +10,30 @@ const ChannelDetail = () => {
   const [channelDetail, setChannelDetail] = useState(null);
   const [videos, setVideos] = useState([]);
 
-console.log(channelDetail, videos)
   useEffect ( () =>{
     YoutubeApi(`channels?part=snippet&id=${id}`).then( (data) =>setChannelDetail(data?.items[0]))
 
     YoutubeApi(`search?channelId=${id}&part=snippet&order=date`).then( (data) =>setVideos(data?.items))
 
   }, [id])
-  return (
-    <div>ChannelDetail</div>
+  return (<>
+    {/* {console.log(channelDetail?.brandingSettings?.channel?.title)} */}
+
+    <Box minHeight={"95vh"} margin = "auto">
+      <Box>
+      <div style={{
+        // backgroundImage : `url(${channelDetail?.brandingSettings?.channel?.image?.bannerExternalUrl})`, 
+      height  : "300px",zIndex : "10",
+      background: "red",
+     color : "white",
+     position : "relative"}} />
+      {/* <ChannelCard channelDetail = {channelDetail} /> */}
+      </Box>
+       {/* <div style = {{position : "absolute", margin: "auto",right: "50%",top: "20rem",zIndex: "20", left: "50%",background:"white", display: "block", width : "auto"}}> */}
+        <ChannelCard channelDetail = {channelDetail} />
+        {/* </div> */}
+    </Box>
+    </>
   )
 }
 
